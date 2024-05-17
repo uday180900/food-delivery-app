62598fae569aeeb1364611c8f3f73369537765b1
“EFLQY” <vasa.udaykiran.ext@bayer.com>
2024-05-17 19:36:23 +0530

    Added header and menu list as pressable

---
+++ src/pages/PlaceOrder/PlaceOrder.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './PlaceOrder.css'
+
+const PlaceOrder = () => {
+  return (
+    <div className='place-order'>
+      
+    </div>
+  )
+}
+
+export default PlaceOrder
